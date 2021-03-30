94eb4b6f759cfaf4b80afb1b7a101b2c21196eb3
Aaron Johnson <acjohnson@pcdomain.com>
2021-03-30 17:19:45 -0500

    fix sleep command

--- plexupdatecert/entrypoint.sh
+++ plexupdatecert/entrypoint.sh
@@ -13,4 +13,4 @@ set -e -o pipefail
     -i /$PLEX_HOST_SSH_KEY \
     -q $PLEX_HOST_USER@$PLEX_HOST_IP "/bin/systemctl restart plexmediaserver; /bin/systemctl status plexmediaserver"
 
-/usr/bin/sleep infinity
+/bin/sleep infinity
